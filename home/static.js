2d9b49ae6d96b97b1714dec1d43577944bd6145e
Edvin Larsson <el223na@student.lnu.se>
2021-12-01 10:11:07 +0100

    Show/hide patient info (#2)
    * changes btn on hover
    
    * info btn is now btn
    
    * tooltip styling in progress
    
    * initial tooltip styling
    
    * rm tooltip
    
    * changes btn text on hover
    
    * detects close
    
    * card overlay
    
    * escape handling
    
    * corner btn
    
    * btn styling
    
    * styling
    
    * conditional backdrop
    
    * close close btn
    
    * no close btn
    
    * iframe src dynamic

--- home/static.js
+++ home/static.js
@@ -2,8 +2,33 @@ const id = (elementId) => document.getElementById(elementId);
 
 const app = id('app-iframe');
 
-if (window.location.origin.includes('file')) {
+const locationPath = window.location.pathname || window.location.origin;
+
+if (locationPath.includes('iframe-poc')) {
   app.setAttribute('src', 'http://localhost:3000/app');
 } else {
   app.setAttribute('src', '/app');
 }
+
+const patientInfoWrap = id('patient-info-wrap');
+
+id('floating-info-button').addEventListener('click', (event) => {
+  event.stopPropagation();
+
+  patientInfoWrap.style.visibility = 'visible';
+
+  const close = () => {
+    patientInfoWrap.style.visibility = 'hidden';
+  };
+
+  document.addEventListener('click', (event) => {
+    const patientInfoCard = id('patient-info');
+    const withinBoundaries = event.composedPath().includes(patientInfoCard);
+
+    if (!withinBoundaries) close();
+  });
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') close();
+  });
+});
